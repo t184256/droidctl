8c3e22ad508a10efeb2868a104383bf97eb2e155
Alexander Sosedkin <monk@unboiled.info>
2024-06-14 16:31:22 +0200

    examples/fdroid: use a different, newer app

--- examples/fdroid.py
+++ examples/fdroid.py
@@ -1,15 +1,15 @@
 def run(d):
     d.fdroid.install('com.simplemobiletools.thankyou',
-                     'de.onyxbits.sensorreadout')
+                     'com.uberspot.a2048')
     pkgs = d.adb.list_packages()
     assert 'com.simplemobiletools.thankyou' in pkgs
-    assert 'de.onyxbits.sensorreadout' in pkgs
+    assert 'com.uberspot.a2048' in pkgs
 
     d.fdroid.uninstall('com.simplemobiletools.thankyou',
-                       'de.onyxbits.sensorreadout')
+                       'com.uberspot.a2048')
     pkgs = d.adb.list_packages()
     assert 'com.simplemobiletools.thankyou' not in pkgs
-    assert 'de.onyxbits.sensorreadout' not in pkgs
+    assert 'com.uberspot.a2048' not in pkgs
 
     # Or, much terser and with rich App objects
     d.fdroid['com.simplemobiletools.thankyou'].uninstall()
